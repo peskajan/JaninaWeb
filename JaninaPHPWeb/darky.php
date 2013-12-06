0360f012200eec0fcefc6d9af76e5147c3ea5ddf
Pesi <Pesi@10.0.0.36>
2013-12-06 17:40:53 +0100

    change page icon

--- JaninaPHPWeb/darky.php
+++ JaninaPHPWeb/darky.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-        <link rel="shortcut icon" href="/icons/favicon.png">
+        <link rel="shortcut icon" href="/icons/title-icon.png">
 
         <title>Dárky, poukazy | Jana Budková |  Líèení</title>
 
